94bdc0a9217a70d0d09bef301d75792afe9958e7
b-rimel <luca.afev78@gmail.com>
2023-08-22 14:38:57 +0200

    Setup de la banner

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Home from "./components/Home/Home.jsx";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
       <Footer />
     </div>
   );
---
+++ kasa/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.scss";
+// import "../../background.png";
+
+function Home() {
+  return (
+    <div>
+      <div className="kasa-banner">
+        <p className="kasa-slogan">Chez vous, partout et ailleurs</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
