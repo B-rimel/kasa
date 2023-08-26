0f4600a04b08e11bee78c247aecb5fbb83492e53
b-rimel <luca.afev78@gmail.com>
2023-08-26 14:29:35 +0200

    Implémentation du caroussel

--- kasa/src/components/Caroussel/Caroussel.jsx
+++ kasa/src/components/Caroussel/Caroussel.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 function Caroussel(props) {
-  return <img src={props.image} alt="" />;
+  const image = props.image;
+  let [imageIndex, setImageIndex] = useState(0);
+
+  const nextPicture = () => {
+    setImageIndex(imageIndex === image.length - 1 ? 0 : imageIndex + 1);
+  };
+
+  const previousPicture = () => {
+    setImageIndex(imageIndex === 0 ? image.length - 1 : imageIndex - 1);
+  };
+
+  return (
+    <div>
+      <img src={image[imageIndex]} alt="" />
+      <p onClick={previousPicture}>Photo précédente !</p>
+      <p onClick={nextPicture}>Photo suivante !</p>
+    </div>
+  );
 }
 
 export default Caroussel;
